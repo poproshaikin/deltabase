4a68a1fe4b6788d3e902f7986f8a3c5d698e70d5
Stanislav <stasstrong06@gmail.com>
2024-10-10 09:33:27 +0200

    Completed

--- Database/Db/Sql/Queries/CreateTableQuery.cs
+++ Database/Db/Sql/Queries/CreateTableQuery.cs
@@ -1,10 +1,12 @@
+using Sql.Expressions;
 using Sql.Tokens;
 
-namespace Sql.Common.Queries;
+namespace Sql.Queries;
 
 public class CreateTableQuery : SqlQuery, IQueryWithTableName
 {
-    public SqlToken TableName { get; init; }
+    public SqlToken TableName { get; set; }
+    public ColumnDefExpression[] NewColumns { get; set; }
        
     public CreateTableQuery(IReadOnlyList<SqlToken> tokens) : base(tokens)
     {
