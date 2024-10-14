7e0af41945188dab96a7286cca3f8e67c315649b
Stanislav <stasstrong06@gmail.com>
2024-10-14 11:57:09 +0200

    Minor changes

--- Database/Utils/ParseHelper.cs
+++ Database/Utils/ParseHelper.cs
@@ -8,6 +8,6 @@ namespace Utils;
 public static class ParseHelper
 {
     public static byte[] GetBytes(string message) => Encoding.UTF8.GetBytes(message);
-    public static byte[] GetBytes(TcpResponseType type) => GetBytes(((int)type).ToString());
+    public static byte[] GetBytes(ResponseType type) => GetBytes(((int)type).ToString());
     public static string GetString(byte[] buffer) => Encoding.UTF8.GetString(buffer);
 }
