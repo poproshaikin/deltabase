e9c25a88c591e6862cdacab2011ab773693b9853
Stanislav <stasstrong06@gmail.com>
2024-10-15 21:11:37 +0200

    Now for the initialization server needs only a string containing server name, reads the server's port and the Tcp listener will be initialized with a read port

--- Database/Server/Server.Core/DltServer.cs
+++ Database/Server/Server.Core/DltServer.cs
@@ -19,19 +19,11 @@ public class DltServer
     
     private Dictionary<DltClient, DltDatabase> _clients;
     
-    private DltConnectionConfig _connectionConfig;
-    private string _serverName;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="DltServer"/> class.
-    /// </summary>
-    /// <param name="cnnConfig">The connection configuration for the server.</param>
-    public DltServer(DltConnectionConfig cnnConfig)
+    public DltServer(string serverName)
     {
-        _serverName = cnnConfig.Server;
-        _connectionConfig = cnnConfig;
-        _fs = new FileSystemManager(_serverName);
-        _listener = new DltTcpListener(cnnConfig);
+        _fs = new FileSystemManager(serverName);
+        DltConnectionConfig config = DltConnectionConfig.Parse(_fs.ReadServerConnectionConfigFile());
+        _listener = new DltTcpListener(config);
         _semaphore = new SemaphoreSlim(initialCount: 1, maxCount: connected_clients_limit);
         _clients = new Dictionary<DltClient, DltDatabase>();
     }
@@ -90,73 +82,140 @@ public class DltServer
         {
             case TcpCommandType.connect:
             {
-                bool authorized = VerifyRequest(request);
-
-                if (authorized)
-                {
-                    client.Authorized = true;
-                    client.RequestsAccepted++;
-
-                    _clients[client] = DltDatabaseInitializer.Init(request.GetConnectionConfig().Database!, _fs) ?? 
-                                       throw new InvalidOperationException(
-                                           $"The database named \"{request.GetConnectionConfig().Database}\" wasn't found");
-
-                    handler.ConnectionConfig = request.GetConnectionConfig();
-                    handler.Write(ResponseType.ConnectedSuccessfully);
-                    return;
-                }
-                else
-                {
-                    handler.Write(ResponseType.Unauthorized);
-                    handler.Dispose();
-                    return;
-                }
+                await ExecuteConnectAsync(client, handler, request);
+                return;
             }
             case TcpCommandType.sql when !client.Authorized:
             {
-                handler.Write(ResponseType.Unauthorized);
-                handler.Dispose();
+                await ExecuteSqlWhenNotAuthorizedAsync(client, handler, request);
                 return;
             }
             case TcpCommandType.sql:
             {
-                string sql = request.Data;
-                byte[] result = _clients[client].ExecuteRequest(sql);
-                
-                handler.Write(result);
+                await ExecuteSqlAsync(client, handler, request);
+                return;
+            }
+            case TcpCommandType.createdatabase:
+            {
+                await ExecuteCreateDatabaseAsync(client, handler, request);
                 return;
             }
             case TcpCommandType.close:
             {
-                _clients.Remove(client);
-                
-                handler.Write(ResponseType.Success);
-                handler.Dispose();
-                
+                await ExecuteCloseAsync(client, handler, request);
                 return;
             }
         }
     }
 
     /// <summary>
-    /// Initializes a new <see cref="DltServer"/> using the provided connection string.
+    /// Asynchronously handles the connection process, authorizing the client and initializing the database connection if authorized.
     /// </summary>
-    /// <param name="connectionString">The connection string for the server configuration.</param>
-    /// <returns>A new instance of the <see cref="DltServer"/> class.</returns>
-    public static DltServer Init(string connectionString)
+    /// <param name="client">The <see cref="DltClient"/> attempting to connect.</param>
+    /// <param name="handler">The <see cref="DltTcpHandler"/> responsible for communication.</param>
+    /// <param name="request">The <see cref="TcpRequest"/> containing connection details.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if the specified database is not found.</exception>
+    private async Task ExecuteConnectAsync(DltClient client, DltTcpHandler handler, TcpRequest request)
     {
-        var connectionConfig = DltConnectionConfig.Parse(connectionString);
-        return new DltServer(connectionConfig);
+        // TODO реализовать способы авторизации
+        bool authorized = true;
+                
+        if (authorized)
+        {
+            client.Authorized = true;
+            client.RequestsAccepted++;
+
+            _clients[client] = DltDatabaseInitializer.Init(request.GetConnectionConfig().Database!, _fs) ?? 
+                               throw new InvalidOperationException(
+                                   $"The database named \"{request.GetConnectionConfig().Database}\" wasn't found");
+
+            handler.ConnectionConfig = request.GetConnectionConfig();
+            await handler.WriteAsync(ResponseType.ConnectedSuccessfully);
+        }
+        else
+        {
+            await handler.WriteAsync(ResponseType.Unauthorized);
+            handler.Dispose();
+        }
     }
-        
+
     /// <summary>
-    /// Verifies if the request matches the server's connection configuration.
+    /// Asynchronously handles SQL requests when the client is not authorized, responding with an unauthorized message.
     /// </summary>
-    /// <param name="request">The request to verify.</param>
-    /// <returns><c>true</c> if the request is authorized; otherwise, <c>false</c>.</returns>
-    private bool VerifyRequest(TcpRequest request)
+    /// <param name="client">The <see cref="DltClient"/> attempting to execute the request.</param>
+    /// <param name="handler">The <see cref="DltTcpHandler"/> responsible for communication.</param>
+    /// <param name="request">The <see cref="TcpRequest"/> containing the SQL request details.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    private async Task ExecuteSqlWhenNotAuthorizedAsync(DltClient client, DltTcpHandler handler, TcpRequest request)
     {
-        return request.GetConnectionConfig() == _connectionConfig;
+        await handler.WriteAsync(ResponseType.Unauthorized);
+        handler.Dispose();
+    }
+
+    
+    /// <summary>
+    /// Asynchronously handles SQL request execution, running the provided SQL command and returning the result.
+    /// </summary>
+    /// <param name="client">The <see cref="DltClient"/> executing the SQL request.</param>
+    /// <param name="handler">The <see cref="DltTcpHandler"/> responsible for communication.</param>
+    /// <param name="request">The <see cref="TcpRequest"/> containing the SQL command.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    private async Task ExecuteSqlAsync(DltClient client, DltTcpHandler handler, TcpRequest request)
+    {
+        string sql = request.Data;
+        byte[] result = _clients[client].ExecuteRequest(sql);
+                
+        await handler.WriteAsync(result);
+    }
+    
+    /// <summary>
+    /// Asynchronously creates a new database, initializing its folders and configuration files.
+    /// </summary>
+    /// <param name="client">The <see cref="DltClient"/> requesting database creation.</param>
+    /// <param name="handler">The <see cref="DltTcpHandler"/> responsible for communication.</param>
+    /// <param name="request">The <see cref="TcpRequest"/> containing the database name.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    private async Task ExecuteCreateDatabaseAsync(DltClient client, DltTcpHandler handler, TcpRequest request)
+    {
+        string dbName = request.Data;
+        
+        _fs.CreateDbFolder(dbName);
+        await _fs.CreateDbConfigFileAsync(dbName);
+        _fs.CreateRecordsFolder(dbName);
+        
+        await handler.WriteAsync(ResponseType.Success);
+    }
+    
+    /// <summary>
+    /// Asynchronously handles the closure of the connection, removing the client and responding with a success message.
+    /// </summary>
+    /// <param name="client">The <see cref="DltClient"/> to be disconnected.</param>
+    /// <param name="handler">The <see cref="DltTcpHandler"/> responsible for communication.</param>
+    /// <param name="request">The <see cref="TcpRequest"/> related to the closure request.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    private async Task ExecuteCloseAsync(DltClient client, DltTcpHandler handler, TcpRequest request)
+    {
+        _clients.Remove(client);
+
+        await handler.WriteAsync(ResponseType.Success);
+        handler.Dispose();
+    }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="DltServer"/>, initializing necessary server folders and configuration files.
+    /// </summary>
+    /// <param name="serverName">The name of the server to be created.</param>
+    /// <param name="password">The password to be used for server authentication.</param>
+    /// <returns>A new instance of <see cref="DltServer"/>.</returns>
+    public static DltServer Create(string serverName, ushort port, string password)
+    {
+        DltServer server = new(serverName);
+        server._fs.CreateServerFolder();
+        server._fs.CreateServerConfigFile(port, password);
+        server._fs.CreateDbsFile();
+        server._fs.CreateDbInServerFolder();
+        return server;
     }
 }
 
