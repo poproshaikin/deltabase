ff32ad9f0d515629a2bcd3232f646e46bbf47bf9
Stanislav <stasstrong06@gmail.com>
2024-10-16 15:25:08 +0200

    All virtual methods are now common

--- Database/Server/Server.Tcp/DltTcpService.cs
+++ Database/Server/Server.Tcp/DltTcpService.cs
@@ -28,7 +28,7 @@ public abstract class DltTcpService
     /// <param name="client">The <see cref="TcpClient"/> whose <see cref="NetworkStream"/> will be used to send the data.</param>
     /// <param name="buffer">The byte array containing the message to write to the stream.</param>
     /// <exception cref="ArgumentNullException">Thrown if the client is null.</exception>
-    protected virtual void Write(TcpClient client, byte[] buffer)
+    protected void Write(TcpClient client, byte[] buffer)
     {
         ArgumentNullException.ThrowIfNull(client, nameof(client));
         
@@ -43,8 +43,8 @@ public abstract class DltTcpService
     /// <param name="client">The <see cref="TcpClient"/> whose <see cref="NetworkStream"/> will be used to send the message.</param>
     /// <param name="message">The string message to write to the stream.</param>
     /// <exception cref="ArgumentNullException">Thrown if the client is null.</exception>
-    protected virtual void Write(TcpClient client, string message) =>
-        Write(client, ParseHelper.GetBytes(message));
+    protected void Write(TcpClient client, string message) =>
+        Write(client, ConvertHelper.GetBytes(message));
 
     /// <summary>
     /// Reads data from the <see cref="NetworkStream"/> of the connected <see cref="TcpClient"/> and returns it as a string.
@@ -52,7 +52,7 @@ public abstract class DltTcpService
     /// </summary>
     /// <param name="client">The connected <see cref="TcpClient"/>.</param>
     /// <returns>The data read from the client's <see cref="NetworkStream"/> as a string.</returns>
-    protected virtual string Read(TcpClient client)
+    protected string Read(TcpClient client)
     {
         Task<string> reading = ReadAsync(client, CancellationToken.None); 
         reading.Wait();
@@ -66,7 +66,7 @@ public abstract class DltTcpService
     /// <param name="cancellationToken">The <see cref="CancellationToken"/> to observe while waiting for data.</param>
     /// <returns>A <see cref="Task"/> representing the asynchronous read operation, containing the string read from the network stream.</returns>
     /// <exception cref="ArgumentNullException">Thrown if the client is null.</exception>
-    protected virtual async Task<string> ReadAsync(TcpClient client, CancellationToken cancellationToken)
+    protected async Task<string> ReadAsync(TcpClient client, CancellationToken cancellationToken)
     {
         ArgumentNullException.ThrowIfNull(client);
         
@@ -76,7 +76,7 @@ public abstract class DltTcpService
         byte[] buffer = new byte[client.Available];
         _ = await stream.ReadAsync(buffer, cancellationToken);
         
-        return ParseHelper.GetString(buffer);
+        return ConvertHelper.GetString(buffer);
     }
 
     /// <summary>
