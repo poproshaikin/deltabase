bdc20ebe9ab382d4127754f877286be9d76f8aa4
Stanislav <stasstrong06@gmail.com>
2024-12-21 21:25:41 +0100

    Class archived

--- src/dbms/Data/Encoding/Base64Encoder.cs
+++ src/dbms/Data/Encoding/Base64Encoder.cs
@@ -2,15 +2,18 @@
 
 namespace Data.Encoding;
 
-internal class Base64Encoder : IDataEncoder
-{
-    public string EncodeValue(object value)
-    {
-        return Convert.ToBase64String(ConvertHelper.GetBytes(value.ToString() ?? string.Empty));
-    }
-
-    public string DecodeValue(string value)
-    {
-        return ConvertHelper.GetString(Convert.FromBase64String(value ?? string.Empty));
-    }
-}
+// internal class Base64Encoder : IDataEncoder
+// {
+//     public int BlockSize => 4;
+//     
+//     public string EncodeValue(object value)
+//     {
+//         return Convert.ToBase64String(ConvertHelper.GetBytes(value.ToString() ?? string.Empty));
+//     }
+//
+//     public string DecodeValue(string value)
+//     {
+//         return ConvertHelper.GetString(Convert.FromBase64String(value ?? string.Empty));
+//     }
+//
+// }
