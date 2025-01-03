f6aa6d85334ff08682d9d2503110470d12bd1c4a
Stanislav <stasstrong06@gmail.com>
2025-01-03 16:00:05 +0100

    Renamed, refactored

--- src/dbms/Enums/FileSystem/FileExtensionType.cs
+++
@@ -1,8 +0,0 @@
-namespace Enums.FileSystem;
-
-public enum FileExtension
-{
-    DEF    = 1,
-    RECORD = 2,
-    CONF   = 3,
-}
---
+++ src/dbms/Enums/FileSystem/FileType.cs
@@ -0,0 +1,8 @@
+namespace Enums.FileSystem;
+
+public enum FileType
+{
+    Def    = 1,
+    Record = 2,
+    Conf   = 3,
+}
