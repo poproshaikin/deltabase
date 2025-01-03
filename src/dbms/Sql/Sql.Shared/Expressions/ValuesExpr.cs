731a860f50563142f9752dc293172a61eaa2e3df
Stanislav <stasstrong06@gmail.com>
2025-01-03 16:05:28 +0100

    added string[](ValuesExpr) operator

--- src/dbms/Sql/Sql.Shared/Expressions/ValuesExpr.cs
+++ src/dbms/Sql/Sql.Shared/Expressions/ValuesExpr.cs
@@ -5,4 +5,6 @@ public class ValuesExpr : SqlExpr
     public string[] Values { get; set; }
 
     public string this[int index] => Values[index];
+    
+    public static implicit operator string[](ValuesExpr valuesExpr) => valuesExpr.Values;
 }
