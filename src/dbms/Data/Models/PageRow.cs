fe7b7aa0c0c7144d2785058b4eb0ee21352e2b8e
poproshaikin <stasstrong06@gmail.com>
2025-02-05 19:44:50 +0100

    now carrying string[] Data instead of object[] Data

--- src/dbms/Data/Models/PageRow.cs
+++ src/dbms/Data/Models/PageRow.cs
@@ -2,24 +2,22 @@ using Enums.Records.Columns;
 
 namespace Data.Models;
 
-public class PageRow
+public struct PageRow
 {
-    internal int RId { get; set; }
+    internal uint RId { get; }
     
-    public object[] Data { get; set; }
+    public string[] Data { get; set; }
+    
+    internal uint ValuesCount => (uint)Data.Length; 
+    
+    public string this[int index] => Data[index];
 
-    public int ColumnsCount => Data.Length;
-    
-    internal int ValuesCount => Data.Length; 
-    
-    public object this[int index] => Data[index];
-
-    public PageRow(int rid, object[] data)
+    public PageRow(int rid, string[] data)
     {
-        RId = rid;
+        RId = (uint)rid;
         Data = data;
     }
-
+    
     public override string ToString()
     {
         return string.Join("■ ", Data);
