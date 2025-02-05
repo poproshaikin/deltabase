cfff5d1868eb82c4c18ff8936370eade9ad59f80
poproshaikin <@poproshaikin>
2025-02-05 19:38:07 +0100

    implement of inserting algorithms in progress

--- src/dbms/Data/Operation/DataInserter.cs
+++ src/dbms/Data/Operation/DataInserter.cs
@@ -1,5 +1,6 @@
 using Data.Definitions;
 using Data.Definitions.Schemes;
+using Data.Models;
 using Data.Operation.IO;
 using Enums.FileSystem;
 using Utils;
@@ -11,54 +12,108 @@ public class DataInserter : DataManipulator
     internal DataInserter(string dbName,
         FileSystemHelper fs,
         FileStreamPool pool,
-        DataDefinitor definitor) : base(dbName,
+        DataDescriptor descriptor) : base(dbName,
         fs,
         pool,
-        definitor)
+        descriptor)
     {
-        _cachedHeaders = [];
     }
 
-    private List<PageHeader> _cachedHeaders;
-
-    public void Insert(TableScheme scheme, string[] finalDataSet)
+    private TableScheme? _cachedScheme;
+    
+    public RowInsertionResult Insert(TableScheme scheme, string[] finalDataSet)
     {
-        Task.Run(InsertAsync(scheme, finalDataSet).Wait);
-    }
-
-    public async Task InsertAsync(TableScheme scheme, string[] finalDataSet) 
-    {
-        // найти свободное место на страницах или создать новую
-        // если свободное место: seek, обновление количества колонок и каждого значения 
-        // если новая: запись новой строки
-
-        FileInfo[] pages = _definitor.GetTableFiles(scheme.TableName, FileType.Record);
-        var pageWithEmptyRows = await TryFindPageWithEmptyRowsAsync(pages);
-
-        if (pageWithEmptyRows is { } page)
+        return Task.Run(() =>
         {
-            BinaryDataIO io = new BinaryDataIO(page.Stream, scheme);
-            int emptyRowId = page.Header.FreeRows[0];
-            
-            // header is already read in the TryFindPageWithEmptyRows method
-            await io.SeekToRowAsync(emptyRowId);
-        }
+            var task = InsertAsync(scheme, finalDataSet);
+            task.Wait();
+            return task.Result;
+        }).Result;
     }
 
-    private async Task<(PageHeader Header, FileStream Stream)?> TryFindPageWithEmptyRowsAsync(FileInfo[] pages)
+    public async Task<RowInsertionResult> InsertAsync(TableScheme scheme, string[] finalDataSet) 
+    {
+        _cachedScheme = scheme;
+        
+        FileInfo[] pages = Descriptor.GetTableFiles(scheme.TableName, FileType.Record);
+        
+        // 1: insert to an empty row
+        // 2: insert to an end of file
+        // 3: create new file 
+
+        var formatter = new BinaryDataFormatter();
+        ulong size = formatter.EstimateSize(scheme, finalDataSet);
+        
+        var context = await PrepareToRowInsertionAsync(pages, size);
+        if (context is null)
+            return RowInsertionResult.FailedToSelectInsertionFile;
+        
+        await ProcessInsertionAsync(context, );
+    }
+
+    private async Task<RowInsertionContext?> PrepareToRowInsertionAsync(FileInfo[] pages, ulong size)
     {
         foreach (FileInfo page in pages)
         {
-            FileStream pageStream = _pool.GetOrOpen(page);
-            BinaryDataIO io = new BinaryDataIO(pageStream);
-            
+            FileStream stream = _pool.GetOrOpen(page);
+            BinaryDataIO io = new(stream);
             PageHeader header = await io.ReadHeaderAsync();
-            if (header.FreeRows.Length > 0)
-            {
-                return (header, pageStream);
-            }
-        }
 
-        return null;
+            if (header.FreeRows.Length != 0)
+            {
+                int rowId = header.FreeRows[0];
+                await io.SeekToRowAsync(rowId, _cachedScheme!, true);
+                return new RowInsertionContext(header, page, stream, RowInsertionOption.Inserting, rowId);
+            }
+
+            if (header.Size + size <= MAXIMUM_PAGE_SIZE)
+            {
+                int rowsCount = header.RowsCount;
+                io.SeekToEnd();
+                return new RowInsertionContext(header, page, stream, RowInsertionOption.Appending, newRowId: rowsCount);
+            }
+            
+            ...
+        }
     }
+
+    private async Task ProcessInsertionAsync(RowInsertionContext context, string[] data)
+    {
+        
+    }
+}
+
+internal readonly struct RowInsertionContext
+{
+    internal PageHeader Header { get; init; }
+    
+    internal FileInfo File { get; init; }
+    
+    internal FileStream Stream { get; init; }
+    
+    internal RowInsertionOption Option { get; init; }
+    
+    internal int NewRowId { get; init; }
+    
+    internal RowInsertionContext(PageHeader header, FileInfo file, FileStream stream, RowInsertionOption option, int newRowId)
+    {
+        Header = header;
+        File = file;
+        Stream = stream;
+        Option = option;
+        NewRowId = newRowId;
+    }
+}
+
+public enum RowInsertionResult
+{
+    Success,
+    FailedToSelectInsertionFile, 
+}
+
+internal enum RowInsertionOption
+{
+    Inserting,
+    Appending,
+    CreatingNewPage,
 }
