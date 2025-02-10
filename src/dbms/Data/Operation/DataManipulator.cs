1e1405e331703b07634aad91b0da3382034998e6
poproshaikin <@poproshaikin>
2025-02-10 09:10:01 +0100

    remove due to redundancy

--- src/dbms/Data/Operation/DataManipulator.cs
+++ src/dbms/Data/Operation/DataManipulator.cs
@@ -1,27 +1,27 @@
-using Utils;
-
-namespace Data.Operation;
-
-public abstract class DataManipulator
-{
-    protected const long MAXIMUM_PAGE_SIZE = 8 * 1024;
-    
-    protected readonly string _dbName;
-
-    protected readonly FileSystemHelper FsHelper;
-    
-    protected readonly DataDescriptor Descriptor;
-    
-    private protected readonly FileStreamPool Pool;
-    
-    private protected DataManipulator(string dbName,
-        FileSystemHelper fsHelper,
-        FileStreamPool pool,
-        DataDescriptor descriptor)
-    {
-        _dbName = dbName;
-        FsHelper = fsHelper;
-        Descriptor = descriptor;
-        Pool = pool;
-    }
-}
+// using Utils;
+//
+// namespace Data.Operation;
+//
+// public abstract class DataManipulator
+// {
+//     protected const long MAXIMUM_PAGE_SIZE = 8 * 1024;
+//     
+//     protected readonly string _dbName;
+//
+//     protected readonly FileSystemHelper FsHelper;
+//
+//     protected readonly DataDescriptor Descriptor;
+//
+//     private protected readonly DataPageManager PageManager;
+//     
+//     private protected DataManipulator(string dbName,
+//         FileSystemHelper fsHelper,
+//         DataStreamPool pool,
+//         DataDescriptor descriptor)
+//     {
+//         _dbName = dbName;
+//         FsHelper = fsHelper;
+//         Descriptor = descriptor;
+//         Pool = pool;
+//     }
+// }
