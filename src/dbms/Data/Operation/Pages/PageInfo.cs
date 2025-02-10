b412d25d852a6b02fc816f68ee0d4fa24490de0f
poproshaikin <@poproshaikin>
2025-02-10 09:13:13 +0100

    rename and add path

--- src/dbms/Data/Operation/Pages/PageInfo.cs
+++ src/dbms/Data/Operation/Pages/PageInfo.cs
@@ -2,18 +2,20 @@ using Data.Definitions.Schemes;
 
 namespace Data.Operation;
 
-public class TableFileMetadata
+public class PageInfo
 {
     public string FileName => FileInfo.Name;
     
     public ulong FileSize => (ulong)FileInfo.Length;
+
+    public string Path => FileInfo.FullName;
     
     public FileInfo FileInfo { get; set; }
     
     public TableScheme Scheme { get; set; }
-    
-    
-    public TableFileMetadata(FileInfo fileInfo, TableScheme scheme)
+
+
+    public PageInfo(FileInfo fileInfo, TableScheme scheme)
     {
         FileInfo = fileInfo;
         Scheme = scheme;
